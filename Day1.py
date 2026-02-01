b30bbabae55bdb266afba23d349483db45cdb853
vrao <k.vaishnavirao16@gmail.com>
2026-02-01 18:21:11 +0530

    Day 1 :Python setup and Basics

---
+++ Day1.py
@@ -0,0 +1,8 @@
+#print("Hi, I'm starting my AI Journey today!")
+
+name = "Anil S A"
+age = 24
+is_learning_ai = True
+
+print ("My name is " + name+" and my age is " +str(age)+"." "I'm starting my AI Journey today..!")
+
